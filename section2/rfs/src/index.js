78c0072a4f543cca4f17265fb58ad6dbf10bfe02
setsunaco <setsunacode@gmail.com>
2018-06-22 14:10:43 +0200

    parent component and work with API

--- section2/rfs/src/index.js
+++ section2/rfs/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CardList from './CardList';
 import './index.css';
-import Hello from './Hello';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';    // to work off line
 import 'tachyons';
+import {robots} from './robots';
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Hello greeting={'Ace also!'}/>, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <CardList robots={robots}/>
+    </div>,
+    document.getElementById('root'));
 registerServiceWorker();
---
+++ section2/rfs/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Brecht',
+        email: 'leanne.graham@mail.cc'
+    },
+    {
+        id: 2,
+        name: 'Sweet Heart',
+        username: 'Sweetyh',
+        email: 'sweet.heart@mail.cc'
+    },
+    {
+        id: 3,
+        name: 'Honey Poop',
+        username: 'Poopy',
+        email: 'honey.poop@mail.cc'
+    },
+    {
+        id: 4,
+        name: 'Glad Again',
+        username: 'Glady',
+        email: 'glad.again@mail.cc'
+    },
+    {
+        id: 5,
+        name: 'Holly Molly',
+        username: 'Ymloh',
+        email: 'holly.molly@mail.cc'
+    },
+    {
+        id: 6,
+        name: 'Nope Toobad',
+        username: 'NopyNope',
+        email: 'nope.toobad@mail.cc'
+    },
+    {
+        id: 7,
+        name: 'Shitty Blast',
+        username: 'Blastou',
+        email: 'shitty.blast@mail.cc'
+    },
+    {
+        id: 8,
+        name: 'Conor Stan',
+        username: 'cetanimal',
+        email: 'conor.stan@mail.cc'
+    },
+    {
+        id: 9,
+        name: 'Monkey Di',
+        username: 'Dynastaurus',
+        email: 'monkey.di@mail.cc'
+    },
+    {
+        id: 10,
+        name: 'Bang Cloud',
+        username: 'Bangg',
+        email: 'bang.cloud@mail.cc'
+    },
+];
