5f109167151d849afde001bfdfc9e5744366086d
setsunaco <setsunacode@gmail.com>
2018-08-09 14:27:31 +0200

    cleaner code and structure

--- section2/rfs/src/containers/App.js
+++ section2/rfs/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './Searchbox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/Searchbox';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
     constructor() {
@@ -19,20 +19,18 @@ class App extends Component {
     }
 
     onSearchChange = (event) => {
-        // console.log(event.target.value);
         this.setState( {searchfield: event.target.value} );
-        // console.log(filteredRobots);
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
-        if (this.state.robots.length === 0) {
-            return <h1> Loading </h1>;
-        } else {
-            return (
+        return !robots.length ?
+             <h1> Loading </h1> :
+             (
                 <div className="tc">
                     <h1 className="f1">Robofriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -41,8 +39,6 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-        }
-
     }
 }
 
