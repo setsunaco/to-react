38544d2cc956468cc31f85921295a20bd7a761bf
setsunaco <setsunacode@gmail.com>
2018-08-09 12:52:31 +0200

    lifecycle componentDidMount + fetch

--- section2/rfs/src/App.js
+++ section2/rfs/src/App.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './Searchbox';
-import { robots } from './robots';
+// import { robots } from './robots';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users })          )
     }
 
     onSearchChange = (event) => {
@@ -23,13 +29,18 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
 
-        return (
-            <div className="tc">
-                <h1 className="f1">Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1> Loading </h1>;
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+
     }
 }
 
