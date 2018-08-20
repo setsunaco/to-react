8179c17d0dfde63298b746ce199871f50b36ae32
setsunaco <setsunacode@gmail.com>
2018-08-21 00:47:02 +0200

    logger middleware

--- section2/rfs/src/index.js
+++ section2/rfs/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker';    // to work off line
@@ -9,8 +10,9 @@ import 'tachyons';
 import { searchRobots } from './reducers';
 
 
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 // const store = createStore(rootReducer);
-const store = createStore(searchRobots);
 
 ReactDOM.render(
     <Provider store={store}>
